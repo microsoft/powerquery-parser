20b62a08a7ef473063e27c88e27eb4af8da66d85
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-04-03 21:11:51 -0700

    reverting commit

--- src/example.ts
+++ src/example.ts
@@ -2,9 +2,7 @@ import { ResultKind } from "./common";
 import { lexAndParse } from "./jobs";
 
 // an example on how to consume the package.
-const document = `
-#time
-`;
+const document = `if true then x else y`;
 const lexResult = lexAndParse(document);
 if (lexResult.kind === ResultKind.Ok) {
     console.log(JSON.stringify(lexResult.value, null, 4));
