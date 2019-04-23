fc254b4e7d6b20e28e9c28e042885fdcd7fc1526
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-04-22 20:04:35 -0700

    more tests

--- src/test/lexer/multilineTokens.ts
+++ src/test/lexer/multilineTokens.ts
@@ -17,33 +17,104 @@ function expectWrappedAbridgedTokens(
     return expectAbridgedTokens(document, separator, expected);
 }
 
-describe(`Lexer.MultilineTokens.StringLiteral`, () => {
-    it(`"X": `, () => {
-        const separator = "\n";
-        const document = `"X"`;
-        const expected: ReadonlyArray<[TokenKind, string]> = [
-            [TokenKind.StringLiteral, `"X"`],
-        ];
-        expectWrappedAbridgedTokens(document, separator, expected);
-    });
+describe(`Lexer`, () => {
+    describe(`MultilineTokens`, () => {
+        describe(`MultilineComment`, () => {
+            it(`/**/`, () => {
+                const separator = "\n";
+                const document = `/**/`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.MultilineComment, `/**/`],
+                ];
+                expectWrappedAbridgedTokens(document, separator, expected);
+            });
 
-    it(`"X\\nX\\nX"`, () => {
-        const seperator = "\n";
-        const document = `"X${seperator}X${seperator}X"`;
-        const expected: ReadonlyArray<[TokenKind, string]> = [
-            [TokenKind.StringLiteral, `"X${seperator}X${seperator}X"`],
-        ];
-        expectWrappedAbridgedTokens(document, seperator, expected);
-    });
+            it(`/* */`, () => {
+                const seperator = "\n";
+                const document = `/* */`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.MultilineComment, `/* */`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
 
-    it(`"X\\nX\\nX" abc`, () => {
-        const seperator = "\n";
-        const document = `"X${seperator}X${seperator}X" abc`;
-        const expected: ReadonlyArray<[TokenKind, string]> = [
-            [TokenKind.StringLiteral, `"X${seperator}X${seperator}X"`],
-            [TokenKind.Identifier, `abc`],
-        ];
-        expectWrappedAbridgedTokens(document, seperator, expected);
-    });
+            it(`/* X */`, () => {
+                const seperator = "\n";
+                const document = `/* X */`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.MultilineComment, `/* X */`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
 
+            it(`/*X\\nX\\nX*/`, () => {
+                const seperator = "\n";
+                const document = `/*X${seperator}X${seperator}X*/`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.MultilineComment, `/*X${seperator}X${seperator}X*/`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
+
+            it(`abc /*X\\nX\\nX*/`, () => {
+                const seperator = "\n";
+                const document = `abc /*X${seperator}X${seperator}X*/`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.Identifier, `abc`],
+                    [TokenKind.MultilineComment, `/*X${seperator}X${seperator}X*/`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
+
+            it(`/*X\\nX\\nX*/ abc`, () => {
+                const seperator = "\n";
+                const document = `/*X${seperator}X${seperator}X*/ abc`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.MultilineComment, `/*X${seperator}X${seperator}X*/`],
+                    [TokenKind.Identifier, `abc`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
+        })
+
+        describe(`StringLiteral`, () => {
+            it(`"X"`, () => {
+                const separator = "\n";
+                const document = `"X"`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.StringLiteral, `"X"`],
+                ];
+                expectWrappedAbridgedTokens(document, separator, expected);
+            });
+
+            it(`"X\\nX\\nX"`, () => {
+                const seperator = "\n";
+                const document = `"X${seperator}X${seperator}X"`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.StringLiteral, `"X${seperator}X${seperator}X"`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
+
+            it(`abc "X\\nX\\nX"`, () => {
+                const seperator = "\n";
+                const document = `abc "X${seperator}X${seperator}X"`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.Identifier, `abc`],
+                    [TokenKind.StringLiteral, `"X${seperator}X${seperator}X"`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
+
+            it(`"X\\nX\\nX" abc`, () => {
+                const seperator = "\n";
+                const document = `"X${seperator}X${seperator}X" abc`;
+                const expected: ReadonlyArray<[TokenKind, string]> = [
+                    [TokenKind.StringLiteral, `"X${seperator}X${seperator}X"`],
+                    [TokenKind.Identifier, `abc`],
+                ];
+                expectWrappedAbridgedTokens(document, seperator, expected);
+            });
+        })
+    })
 });
