c96d9ee54227cd904124c9a47e8a38ee83b8960f
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-04-24 01:49:27 -0700

    updating tests

--- src/test/lexer/common.ts
+++ src/test/lexer/common.ts
@@ -118,24 +118,3 @@ export function expectAbridgedSnapshot(
     expect(actualComments).deep.equal(expectedComments, JSON.stringify(commentDetails));
     return snapshot;
 }
-
-// export function touchedLexerFactory(): Lexer.TLexer {
-//     const document = "!";
-//     let lexer: Lexer.TLexer = Lexer.from(document);
-//     lexer = Lexer.remaining(lexer);
-
-//     if (lexer.kind !== Lexer.LexerKind.Touched) {
-//         throw new Error(`lexer.kind !== Lexer.LexerKind.Touched: ${JSON.stringify(lexer)}`);
-//     }
-//     return lexer;
-// }
-
-// export function touchedWithErrorLexerFactory(document: string): Lexer.TLexer {
-//     let lexer: Lexer.TLexer = Lexer.from(document);
-//     lexer = Lexer.remaining(lexer);
-
-//     if (!Lexer.hasError(lexer)) {
-//         throw new Error(`expected Lexer.hasError(lexer): ${JSON.stringify(lexer)}`);
-//     }
-//     return lexer;
-// }
