8be67d443ddbcfade8d557885eff2601a19fcfd7
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-05-15 16:40:50 -0700

    lineComments contain newline

--- src/lexer/lexerSnapshot.ts
+++ src/lexer/lexerSnapshot.ts
@@ -111,7 +111,7 @@ function readLineComment(
     return {
         kind: CommentKind.Line,
         data: flatToken.data,
-        containsNewline: positionStart.lineNumber !== positionEnd.lineNumber,
+        containsNewline: true,
         positionStart,
         positionEnd,
     };
