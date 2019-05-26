abb54216ba01ea12658c39f20e0922f26bc79835
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-05-25 22:10:38 -0700

    sort imports

--- src/parser/context.ts
+++ src/parser/context.ts
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
-import { Option, CommonError } from "../common";
-import { Ast } from "./ast";
+import { CommonError, Option } from "../common";
 import { Token } from "../lexer";
+import { Ast } from "./ast";
 
 export namespace ParserContext {
 
