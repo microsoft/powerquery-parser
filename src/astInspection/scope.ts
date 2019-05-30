5d49792d762abe72b66f3d9e6445c3b976cd3879
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-05-29 19:03:01 -0700

    scratch work

--- src/astInspection/scope.ts
+++ src/astInspection/scope.ts
@@ -1,6 +1,6 @@
 import { CommonError, Option } from "../common";
 import { TokenPosition } from "../lexer";
-import { Ast, TokenRange } from "../parser";
+import { Ast, TokenRange, ParserContext } from "../parser";
 
 /* tslint:disable */
 
@@ -9,48 +9,71 @@ interface Position {
     readonly lineCodeUnit: number;
 }
 
-interface State {
+const enum InspectionMode {
+    Ast = "Ast",
+    Context = "Context",
+}
+
+interface InspectionState {
+    // values returned in an instance of Inspection
     scope: string[];
     isIdentifier: boolean;
     isLeftHandAssignment: boolean;
     inRecord: boolean;
     inEach: boolean;
     inFunction: boolean;
-    tokenRange: TokenRange;
+    tokenRangeStart: number;
+    tokenRangeEnd: number;
+
+    // used by inspection functions
+    mode: InspectionMode;
+    astNodesById: Map<number, Ast.TNode>;
+    contextNodesById: Map<number, ParserContext.Node>;
 }
 
-function inspectAstNode(state: State, node: Ast.TNode, nodesById: Map<number, Ast.TNode>) {
-    let maybeNode: Option<Ast.TNode> = node;
+function inspectContextState(contextState: ParserContext.State) {}
 
-    while (maybeNode) {
-        const node: Ast.TNode = maybeNode;
+function inspectionStateFactory(
+    position: Position,
+    astNodesById: Map<number, Ast.TNode>,
+    contextNodesById: Map<number, ParserContext.Node>,
+): InspectionState {}
 
-        switch (node.kind) {
-            case Ast.NodeKind.EachExpression:
-                if (!state.inEach) {
-                    state.scope.push("_");
-                }
-                state.inEach = true;
-                break;
+// function inspectParseOk(parseOk: ParseOk, position: Position) {
+//     const terminalNode: Ast.TNode = expectAstTerminalNode(parseOk.nodesById, parseOk.terminalNodeIds, position);
+// }
 
-            case Ast.NodeKind.GeneralizedIdentifier:
-                state.scope.push(node.literal);
-                break;
+// function inspectAstNode(inspectionState: InspectionState, node: Ast.TNode, nodesById: Map<number, ParserContext.Node>) {
+//     let maybeNode: Option<Ast.TNode> = node;
 
-            case Ast.NodeKind.Identifier:
-                state.scope.push(node.literal);
-                break;
-        }
+//     while (maybeNode) {
+//         const node: Ast.TNode = maybeNode;
 
-        if (node.maybeParentId === undefined) {
-            maybeNode = undefined;
-        }
-        else {
-            const parentId: number = node.maybeParentId;
-            maybeNode = nodesById.get(parentId);
-        }        
-    }
-}
+//         switch (node.kind) {
+//             case Ast.NodeKind.EachExpression:
+//                 if (!inspectionState.inEach) {
+//                     inspectionState.scope.push("_");
+//                 }
+//                 inspectionState.inEach = true;
+//                 break;
+
+//             case Ast.NodeKind.GeneralizedIdentifier:
+//                 inspectionState.scope.push(node.literal);
+//                 break;
+
+//             case Ast.NodeKind.Identifier:
+//                 inspectionState.scope.push(node.literal);
+//                 break;
+//         }
+
+//         if (node.maybeParentId === undefined) {
+//             maybeNode = undefined;
+//         } else {
+//             const parentId: number = node.maybeParentId;
+//             maybeNode = nodesById.get(parentId);
+//         }
+//     }
+// }
 
 function expectAstTerminalNode(
     nodesById: Map<number, Ast.TNode>,
@@ -66,20 +89,57 @@ function expectAstTerminalNode(
         }
         const node: Ast.TNode = maybeNode;
         const tokenRange: TokenRange = node.tokenRange;
-        const positionStart: TokenPosition = tokenRange.positionStart;
-        const positionEnd: TokenPosition = tokenRange.positionEnd;
-
-        const isMatch: boolean =
-            positionStart.lineNumber >= position.lineNumber &&
-            positionEnd.lineNumber <= position.lineNumber &&
-            positionStart.lineCodeUnit >= position.lineCodeUnit &&
-            positionEnd.lineCodeUnit >= position.lineCodeUnit;
-        if (isMatch) {
+        if (isInRange(position, tokenRange)) {
             return node;
         }
     }
 
-    throw new CommonError.InvariantError(
-        `Could not find a terminal node at given position: position=${JSON.stringify(position)}`,
+    return undefined;
+
+    // throw new CommonError.InvariantError(
+    //     `Could not find a terminal node at given position: position=${JSON.stringify(position)}`,
+    // );
+}
+
+function expectTerminalNode(
+    astNodesById: Map<number, Ast.TNode>,
+    contextNodesById: Map<number, ParserContext.Node>,
+    terminalNodeIds: ReadonlyArray<number>,
+    position: Position,
+): Ast.TNode {
+    for (const nodeId of terminalNodeIds) {
+        if (contextNodesById.has(nodeId)) {
+            const contextNode: ParserContext.Node = contextNodesById.get(nodeId) as ParserContext.Node;
+            if (contextNode.maybeAstNode === undefined) {
+                throw new CommonError.InvariantError(
+                    `maybeAstNode should be truthy. The nodeId=${nodeId} should've been placed in terminalNodeIds as the context was ending.`,
+                );
+            }
+    
+            const astNode: Ast.TNode = contextNode.maybeAstNode;
+            const tokenRange: TokenRange = astNode.tokenRange;
+            if (isInRange(position, tokenRange)) {
+                return astNode;
+            }
+        }
+
+    }
+
+    return undefined;
+
+    // throw new CommonError.InvariantError(
+    //     `Could not find a terminal node at given position: position=${JSON.stringify(position)}`,
+    // );
+}
+
+function isInRange(position: Position, tokenRange: TokenRange): boolean {
+    const positionStart: TokenPosition = tokenRange.positionStart;
+    const positionEnd: TokenPosition = tokenRange.positionEnd;
+
+    return (
+        positionStart.lineNumber >= position.lineNumber &&
+        positionEnd.lineNumber <= position.lineNumber &&
+        positionStart.lineCodeUnit >= position.lineCodeUnit &&
+        positionEnd.lineCodeUnit >= position.lineCodeUnit
     );
 }
