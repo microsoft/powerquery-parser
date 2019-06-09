b2c1081d762eb474df23a642d68859fb22f848be
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-06-08 21:32:47 -0700

    added description

--- src/parser/context.ts
+++ src/parser/context.ts
@@ -4,6 +4,27 @@ import { Ast, NodeIdMap } from ".";
 import { CommonError, Option } from "../common";
 import { Token } from "../lexer";
 
+// Parsing use to be one giant evaluation, leading to an all-or-nothing outcome which was unsuitable for a
+// document that was being live edited.
+//
+// Take the scenario where a user is in the process of adding another element to a ListExpression.
+// Once a comma is typed the parser would error out as it also expects the yet untyped element.
+// Under the one giant evaluation model there was no way to propegate what was parsed up to that point.
+//
+// Context is used as a workbook for Ast.TNodes that have started evaluation but haven't yet finished.
+// It starts out empty, with no children belonging to it.
+// Most (all non-leaf) Ast.TNode's require several sub-Ast.TNodes to be evalauted as well.
+// For each sub-Ast.TNode that begins evaluation another Context is created and linked as a child of the original.
+// This means if a Ast.TNode has N attributes of type Ast.TNode, then the Ast.TNode is fully evaluated there should be N
+// child contexts created belonging under the orignal Context.
+// Once the Ast.TNode evaluation is complete the result is saved on the Context under its maybeAstNode attribute.
+//
+// Back to the scenario listed above, where the user has entered `{1,}`, you could examine the context state to find:
+//  An incomplete ListExpression context with 3 children
+//  With the first child being an evaluated Ast.TNode of NodeKind.Constant: `{`
+//  With the second child being an evaluated Ast.TNode of NodeKind.Csv: `1,`
+//  With the third child being a yet-to-be evaluated Context of NodeKind.Csv
+
 export interface State {
     readonly root: Root;
     readonly nodeIdMapCollection: NodeIdMap.Collection;
