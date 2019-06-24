fca9f9052cc603d87ec68260b076b96b4e981d35
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-06-24 16:01:34 -0700

    cleanup

--- src/inspection/inspectParserContextNodes.ts
+++ src/inspection/inspectParserContextNodes.ts
@@ -27,59 +27,17 @@ export function inspectContextNode(state: State, node: ParserContext.Node): void
         }
 
         case Ast.NodeKind.Identifier:
-            if (!isParentOfNodeKind(state.nodeIdMapCollection, node.id, Ast.NodeKind.IdentifierExpression)) {
-                const maybeAstNode: Option<Ast.Identifier> = maybeCastAsAstNode<
-                    Ast.Identifier,
-                    Ast.NodeKind.Identifier
-                >(node, Ast.NodeKind.Identifier);
-                if (maybeAstNode !== undefined) {
-                    const astNode: Ast.Identifier = maybeAstNode;
-                    addContextToScopeIfNew(state, astNode.literal, node);
-                }
+            if (
+                !isParentOfNodeKind(state.nodeIdMapCollection, node.id, Ast.NodeKind.IdentifierExpression) &&
+                node.maybeAstNode
+            ) {
+                const identifier: Ast.Identifier = node.maybeAstNode as Ast.Identifier;
+                addContextToScopeIfNew(state, identifier.literal, node);
             }
             break;
 
         case Ast.NodeKind.IdentifierExpression: {
-            let identifier: string = "";
-            const children: ReadonlyArray<NodeIdMap.TXorNode> = contextChildren(state.nodeIdMapCollection, node);
-
-            if (children.length === 1 || children.length === 2) {
-                const firstChild: NodeIdMap.TXorNode = children[0];
-                if (firstChild.kind === NodeIdMap.XorNodeKind.Ast) {
-                    switch (firstChild.node.kind) {
-                        // inclusive constant `@`
-                        case Ast.NodeKind.Constant:
-                        // no inclusive constant
-                        case Ast.NodeKind.Identifier:
-                            identifier += firstChild.node.literal;
-                            break;
-
-                        default:
-                            const details: {} = { nodeKind: firstChild.node.kind };
-                            throw new CommonError.InvariantError(
-                                `identifierExpression has invalid Ast.NodeKind`,
-                                details,
-                            );
-                    }
-                }
-            }
-
-            if (children.length === 2) {
-                const secondChild: NodeIdMap.TXorNode = children[1];
-                if (secondChild.kind === NodeIdMap.XorNodeKind.Ast) {
-                    if (secondChild.node.kind !== Ast.NodeKind.Identifier) {
-                        const details: {} = { nodeKind: secondChild.node.kind };
-                        throw new CommonError.InvariantError(`identifierExpression has invalid Ast.NodeKind`, details);
-                    }
-
-                    identifier += secondChild.node.literal;
-                }
-            }
-
-            if (identifier) {
-                addContextToScopeIfNew(state, identifier, node);
-            }
-
+            inspectIdentifierExpression(state, node);
             break;
         }
 
@@ -112,22 +70,6 @@ export function inspectContextNode(state: State, node: ParserContext.Node): void
 
         default:
             break;
-    }
-}
-
-function maybeCastAsAstNode<T, Kind>(
-    contextNode: ParserContext.Node,
-    nodeKind: Ast.NodeKind & Kind,
-): Option<T & Ast.TNode> {
-    if (contextNode.maybeAstNode === undefined) {
-        return undefined;
-    }
-    const astNode: Ast.TNode = contextNode.maybeAstNode;
-
-    if (astNode.kind === nodeKind) {
-        return astNode as T & Ast.TNode;
-    } else {
-        return undefined;
     }
 }
 
@@ -229,4 +171,43 @@ function keysFromRecord(
     }
 
     return keys;
+}
+
+function inspectIdentifierExpression(state: State, node: ParserContext.Node): void {
+    let identifier: string = "";
+    const children: ReadonlyArray<NodeIdMap.TXorNode> = contextChildren(state.nodeIdMapCollection, node);
+
+    if (children.length === 1 || children.length === 2) {
+        const firstChild: NodeIdMap.TXorNode = children[0];
+        if (firstChild.kind === NodeIdMap.XorNodeKind.Ast) {
+            switch (firstChild.node.kind) {
+                // inclusive constant `@`
+                case Ast.NodeKind.Constant:
+                // no inclusive constant
+                case Ast.NodeKind.Identifier:
+                    identifier += firstChild.node.literal;
+                    break;
+
+                default:
+                    const details: {} = { nodeKind: firstChild.node.kind };
+                    throw new CommonError.InvariantError(`identifierExpression has invalid Ast.NodeKind`, details);
+            }
+        }
+    }
+
+    if (children.length === 2) {
+        const secondChild: NodeIdMap.TXorNode = children[1];
+        if (secondChild.kind === NodeIdMap.XorNodeKind.Ast) {
+            if (secondChild.node.kind !== Ast.NodeKind.Identifier) {
+                const details: {} = { nodeKind: secondChild.node.kind };
+                throw new CommonError.InvariantError(`identifierExpression has invalid Ast.NodeKind`, details);
+            }
+
+            identifier += secondChild.node.literal;
+        }
+    }
+
+    if (identifier.length) {
+        addContextToScopeIfNew(state, identifier, node);
+    }
 }
