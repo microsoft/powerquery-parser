7b70f8c52f7a9dd5eec84cc3732d73d22d3be70d
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-08-23 12:25:13 -0700

    missing legal header

--- src/parser/parsers/IParser.ts
+++ src/parser/parsers/IParser.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { Ast, ParserContext } from "..";
 import { Option } from "../../common";
 import { LexerSnapshot, Token, TokenKind } from "../../lexer";
