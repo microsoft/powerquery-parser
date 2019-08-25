5a29573388aff7684fc474c9f6d652d33d769b36
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-08-25 16:16:00 -0700

    added missing license

--- src/parser/parsers/common.ts
+++ src/parser/parsers/common.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
 import { Ast, ParserError } from "..";
 import { CommonError, Option } from "../../common";
 import { Token, TokenKind } from "../../lexer";
