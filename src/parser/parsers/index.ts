9b08597b36864e2269c13aadf38776f1db165c61
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-09-10 11:21:39 -0700

    going to backport this into IParser branch

--- src/parser/parsers/index.ts
+++ src/parser/parsers/index.ts
@@ -1,4 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+import * as Naive from "./naive";
+
 export { RecursiveDescentParser } from "./recursiveDescentParser";
+export { Naive };
