bb5dd7a163e4f1b61d9c86aa61e22540cacfee14
JordanBoltonMN <jordanboltonmn@gmail.com>
2019-10-21 11:35:42 -0700

    fixing test

--- src/test/parser/children.ts
+++ src/test/parser/children.ts
@@ -41,7 +41,7 @@ describe("Parser.Children", () => {
         const text: string = `() as number => 1`;
         const expected: ReadonlyArray<ChildIdsByIdEntry> = [
             {
-                childNodeIds: [2, 6, 10, 18],
+                childNodeIds: [2, 6, 10, 17],
                 id: 1,
                 kind: Ast.NodeKind.FunctionExpression,
             },
