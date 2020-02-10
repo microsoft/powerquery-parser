f40a105124f0401f8582300a569c6c359e004b0f
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-02-10 07:25:03 -0800

    update string format fn

--- src/common/stringUtils.ts
+++ src/common/stringUtils.ts
@@ -85,33 +85,18 @@ export function maybeNewlineKindAt(text: string, index: number): NewlineKind | u
 }
 
 // A quick and dirty way to do string formatting.
-// Continually replaces '{}' until no more exist or you run out of args.
 // Does not handle any escaping.
 export function expectFormat(template: string, args: Map<string, string>): string {
-    throw new Error();
+    let result: string = template;
 
-    // const details: {} = {
-    //     template,
-    //     args: [...args],
-    // };
+    for (const [key, value] of args.entries()) {
+        const formatKey: string = `{${key}}`;
+        if (template.indexOf(formatKey) === -1) {
+            throw new CommonError.InvariantError("unknown key", { formatKey });
+        }
 
-    // let result: string = template;
-    // while (args.length) {
-    //     if (result.indexOf("{}") === -1) {
-    //         throw new CommonError.InvariantError("not enough arguments were given during formatting.", details);
-    //     }
+        result = result.replace(formatKey, value);
+    }
 
-    //     const arg: any = args.pop();
-    //     result = result.replace("{}", arg);
-    // }
-
-    // if (args.length) {
-    //     throw new CommonError.InvariantError("too many arguments were given during formatting.", details);
-    // }
-
-    // return result;
-}
-
-interface FormatOption {
-    [key: string]: string;
+    return result;
 }
