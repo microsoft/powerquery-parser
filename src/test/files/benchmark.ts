5fe3c8f7cea259cb74a1c54dca2ced5b3bfe1470
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-02-19 15:36:00 -0800

    working on additional output

--- src/test/files/benchmark.ts
+++ src/test/files/benchmark.ts
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
+// tslint:disable-next-line: no-require-imports
+import performanceNow = require("performance-now");
+
 import "mocha";
 import { ResultUtils } from "../../common";
 import { LexerSnapshot } from "../../lexer";
@@ -9,17 +12,29 @@ import { IParser, IParserState, IParserStateUtils } from "../../parser";
 import { CombinatorialParser } from "../../parser/parsers";
 import { ParseSettings, Settings } from "../../settings";
 import { TriedLexParse } from "../../tasks";
-import { BenchmarkParser, BenchmarkState } from "./benchmarkParser";
+import { BenchmarkParser, BenchmarkState, FunctionTimestamp } from "./benchmarkParser";
 
 import * as path from "path";
 import * as FileUtils from "../fileUtils";
 
-const parsers: ReadonlyArray<[ParseSettings<BenchmarkState>, string]> = [
+interface FileSummary {
+    readonly fileName: string;
+    readonly numberOfRuns: number;
+    readonly allRunsTimeStart: number;
+    readonly allRunsTimeEnd: number;
+    readonly allRunsTimeDuration: number;
+    readonly singleRunDurationMin: number;
+    readonly singleRunDurationMax: number;
+}
+
+const Parsers: ReadonlyArray<[ParseSettings<BenchmarkState>, string]> = [
     [createBenchmarkParseSettings(createCombinatorialBenchmarkState), "CombinatorialParser"],
     [createBenchmarkParseSettings(createRecurisveDescentBenchmarkState), "RecursiveDescentParser"],
 ];
 
-const headers: ReadonlyArray<string> = [
+const NumberOfRunsPerFile: number = 100;
+
+const RunHeaders: ReadonlyArray<string> = [
     "fileName",
     "id",
     "fnName",
@@ -34,7 +49,17 @@ const headers: ReadonlyArray<string> = [
     "timeDuration",
 ];
 
-for (const [settings, parserName] of parsers) {
+const ReportHeaders: ReadonlyArray<string> = [
+    "fileName",
+    "numRuns",
+    "timeStart",
+    "timeEnd",
+    "timeDuration",
+    "timeDurationMin",
+    "timeDurationMax",
+];
+
+for (const [settings, parserName] of Parsers) {
     parseAllFiles(settings, parserName);
 }
 
@@ -76,37 +101,97 @@ function createBenchmarkParseSettings(
 }
 
 function parseAllFiles(settings: Settings<BenchmarkState>, parserName: string): void {
-    describe(`Benchmark ${parserName} on benchmarkResources directory`, () => {
-        const fileDirectory: string = path.join(path.dirname(__filename), "benchmarkResources");
+    throw new Error()
+    const resourceDirectory: string = path.join(path.dirname(__filename), "benchmarkResources");
+    const summaries: FileSummary[] = [];
 
-        for (const filePath of FileUtils.getPowerQueryFilesRecursively(fileDirectory)) {
-            const fileName: string = path.basename(filePath);
+    for (const filePath of FileUtils.getPowerQueryFilesRecursively(resourceDirectory)) {
+        const fileName: string = path.basename(filePath);
 
-            it(fileName, () => {
-                const triedLexParse: TriedLexParse<BenchmarkState> = FileUtils.tryLexParse(settings, filePath);
-                if (!ResultUtils.isOk(triedLexParse)) {
-                    throw triedLexParse.error;
-                }
+        const timings: Map<number, FunctionTimestamp>[] = [];
+        const allRunsTimeStart: number = performanceNow();
 
-                let csvContent: string = `${headers.join(",")}\n`;
-                for (const fnTimestamp of triedLexParse.value.state.functionTimestamps.values()) {
-                    csvContent += `${fileName}`;
-                    csvContent += `,${fnTimestamp.id}`;
-                    csvContent += `,${fnTimestamp.fnName}`;
-                    csvContent += `,${fnTimestamp.lineNumberStart}`;
-                    csvContent += `,${fnTimestamp.lineCodeUnitStart}`;
-                    csvContent += `,${fnTimestamp.codeUnitStart}`;
-                    csvContent += `,${fnTimestamp.lineNumberEnd}`;
-                    csvContent += `,${fnTimestamp.lineCodeUnitEnd}`;
-                    csvContent += `,${fnTimestamp.codeUnitEnd}`;
-                    csvContent += `,${fnTimestamp.timeStart}`;
-                    csvContent += `,${fnTimestamp.timeEnd}`;
-                    csvContent += `,${fnTimestamp.timeDuration}\n`;
-                }
-
-                const logFilePath: string = path.join(fileDirectory, "logs", `${fileName}.${parserName}.perf`);
-                FileUtils.writeContents(logFilePath, csvContent);
-            });
+        for (let index: number = 0; index < NumberOfRunsPerFile; index += 1) {
+            const triedLexParse: TriedLexParse<BenchmarkState> = FileUtils.tryLexParse(settings, filePath);
+            if (!ResultUtils.isOk(triedLexParse)) {
+                throw triedLexParse.error;
+            }
+            timings.push(triedLexParse.value.state.functionTimestamps);
         }
-    });
+
+        let singleRunDurationMin: number = Number.MAX_SAFE_INTEGER;
+        let singleRunDurationMax: number = Number.MIN_SAFE_INTEGER;
+        for (const fnTimestamps of timings) {
+            for (const entry of fnTimestamps.values()) {
+                // When it switches from reading an expression document to a section documents
+                // it can leave undefined duration values.
+                // This is the easiest way to drop them.
+                if (entry.timeDuration === undefined) {
+                    continue;
+                }
+                singleRunDurationMin = Math.min(singleRunDurationMin, entry.timeDuration);
+                singleRunDurationMax = Math.max(singleRunDurationMax, entry.timeDuration);
+            }
+        }
+
+        const allRunsTimeEnd: number = performanceNow();
+        summaries.push({
+            fileName,
+            numberOfRuns: NumberOfRunsPerFile,
+            allRunsTimeStart,
+            allRunsTimeEnd,
+            allRunsTimeDuration: allRunsTimeEnd - allRunsTimeStart,
+            singleRunDurationMin,
+            singleRunDurationMax,
+        });
+
+        for (let index: number = 0; index < NumberOfRunsPerFile; index += 1) {
+            const perfFileName: string = `${fileName}_${parserName}_${index}.perf`;
+            writeSingleRunTimestamps(resourceDirectory, perfFileName, timings[index]);
+        }
+
+        const reportFileName: string = `${fileName}_${parserName}`;
+        writeReport(resourceDirectory, reportFileName, summaries);
+    }
+}
+
+function writeReport(resourceDirectory: string, reportFileName: string, summaries: ReadonlyArray<FileSummary>): void {
+    let csvContent: string = `${ReportHeaders.join(",")}\n`;
+    for (const summary of summaries) {
+        csvContent += `${summary.fileName}`;
+        csvContent += `,${summary.numberOfRuns}`;
+        csvContent += `,${summary.allRunsTimeStart}`;
+        csvContent += `,${summary.allRunsTimeEnd}`;
+        csvContent += `,${summary.allRunsTimeDuration}`;
+        csvContent += `,${summary.singleRunDurationMin}`;
+        csvContent += `,${summary.singleRunDurationMin}`;
+    }
+
+    const logFilePath: string = path.join(resourceDirectory, "logs", reportFileName);
+    FileUtils.writeContents(logFilePath, csvContent);
+}
+
+function writeSingleRunTimestamps(
+    resourceDirectory: string,
+    perfFileName: string,
+    fnTimestamps: Map<number, FunctionTimestamp>,
+): void {
+    let csvContent: string = `${RunHeaders.join(",")}\n`;
+    for (const fnTimestamp of fnTimestamps.values()) {
+        csvContent += `${perfFileName}`;
+        csvContent += `,${fnTimestamp.id}`;
+        csvContent += `,${fnTimestamp.fnName}`;
+        csvContent += `,${fnTimestamp.lineNumberStart}`;
+        csvContent += `,${fnTimestamp.lineCodeUnitStart}`;
+        csvContent += `,${fnTimestamp.codeUnitStart}`;
+        csvContent += `,${fnTimestamp.lineNumberEnd}`;
+        csvContent += `,${fnTimestamp.lineCodeUnitEnd}`;
+        csvContent += `,${fnTimestamp.codeUnitEnd}`;
+        csvContent += `,${fnTimestamp.timeStart}`;
+        csvContent += `,${fnTimestamp.timeEnd}`;
+        csvContent += `,${fnTimestamp.timeDuration}\n`;
+    }
+
+    const logFilePath: string = path.join(resourceDirectory, "logs", perfFileName);
+    FileUtils.writeContents(logFilePath, csvContent);
 }
