84c83429bff0d12e370d13053fe06f7dd41c1c9d
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-04-07 14:50:14 -0700

    removed dead code

--- src/inspection/index.ts
+++ src/inspection/index.ts
@@ -1,9 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as InspectionUtils from "./inspectionUtils";
-
-export { InspectionUtils };
 export * from "./autocomplete";
 export * from "./position";
 export * from "./scope";
