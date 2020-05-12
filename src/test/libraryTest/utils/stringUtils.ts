44c1e9d451dbb0486693afed382b3cd9f9564523
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-05-12 10:22:26 -0700

    some tests are better than none

---
+++ src/test/libraryTest/utils/stringUtils.ts
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { expect } from "chai";
+import "mocha";
+import { StringUtils } from "../../../common";
+
+describe("StringUtils", () => {
+    describe(`isGeneralizedIdentifier`, () => {
+        it("a", () => expect(StringUtils.isGeneralizedIdentifier("a"), "should be true").to.be.true);
+        it("a.1", () => expect(StringUtils.isGeneralizedIdentifier("a.1"), "should be true").to.be.true);
+        it("a b", () => expect(StringUtils.isGeneralizedIdentifier("a b"), "should be true").to.be.true);
+
+        it("a..1", () => expect(StringUtils.isGeneralizedIdentifier("a..1"), "should be true").to.be.false);
+    });
+});
