9cade2d965c6b5c06e84c8306de4d5a89258516c
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-05-16 12:54:16 -0700

    split valid/invalid tests

--- src/test/libraryTest/utils/stringUtils.ts
+++ src/test/libraryTest/utils/stringUtils.ts
@@ -7,10 +7,13 @@ import { StringUtils } from "../../../common";
 
 describe("StringUtils", () => {
     describe(`isGeneralizedIdentifier`, () => {
-        it("a", () => expect(StringUtils.isGeneralizedIdentifier("a"), "should be true").to.be.true);
-        it("a.1", () => expect(StringUtils.isGeneralizedIdentifier("a.1"), "should be true").to.be.true);
-        it("a b", () => expect(StringUtils.isGeneralizedIdentifier("a b"), "should be true").to.be.true);
-
-        it("a..1", () => expect(StringUtils.isGeneralizedIdentifier("a..1"), "should be false").to.be.false);
+        describe(`valid`, () => {
+            it("a", () => expect(StringUtils.isGeneralizedIdentifier("a"), "should be true").to.be.true);
+            it("a.1", () => expect(StringUtils.isGeneralizedIdentifier("a.1"), "should be true").to.be.true);
+            it("a b", () => expect(StringUtils.isGeneralizedIdentifier("a b"), "should be true").to.be.true);
+        });
+        describe(`invalid`, () => {
+            it("a..1", () => expect(StringUtils.isGeneralizedIdentifier("a..1"), "should be false").to.be.false);
+        });
     });
 });
