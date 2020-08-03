1d3c060ee56218da190116e5e2396de19004df9a
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-08-03 11:04:28 -0700

    updated localization script and ran it

--- scripts/localization_drop.py
+++ scripts/localization_drop.py
@@ -1,17 +1,28 @@
 import os
 import shutil
+import sys
 
-LOCALIZATION_DIR = r"\\simpleloc\drops\Drops\PowerQuery_parser_2490\"
-POWERQUERY_PARSER_DIR = os.path.dirname(os.path.dirname(__file__))
+LOCALIZATION_DIR = r"\\simpleloc\drops\Drops\PowerQuery_parser_2490"
+POWERQUERY_PARSER_DIR = os.path.join(
+    "..",
+    os.path.dirname(os.path.dirname(__file__)),
+)
 
 latest_dir = None
 for entry in os.scandir(LOCALIZATION_DIR):
     if not entry.is_dir():
         continue
-    elif latest_dir is None or latest_dir < entry:
+    elif latest_dir is None or latest_dir.name < entry.name:
         latest_dir = entry
 
 assert latest_dir is not None
+
+with open("localization_drop", "r") as f:
+    if f.read() == latest_dir.name:
+        print("Already picked up {}, exiting.".format(latest_dir.name))
+        sys.exit(0)
+
+print("Picking up a new version, {}".format(latest_dir.name))
 
 LOCALIZATION_DROP_DIR = os.path.join(
     latest_dir,
@@ -48,5 +59,8 @@ for entry in os.scandir(LOCALIZATION_DROP_DIR):
         "{}.json".format(localization_code)
     )
 
-    print("Copying {}".format(localization_code))
+    print("Copying {}\n\tsrc={}\n\tdst={}".format(localization_code, localization_src, localization_dst))
     shutil.copyfile(localization_src, localization_dst)
+
+with open("localization_drop", "w") as f:
+    f.write(latest_dir.name)
