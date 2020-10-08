39a1b235fa55a5d032118262c2855369db83b649
JordanBoltonMN <jordanboltonmn@gmail.com>
2020-10-08 13:32:35 -0700

    updated drop script, and picked up a drop

--- scripts/localization_drop.py
+++ scripts/localization_drop.py
@@ -1,60 +1,46 @@
-# This script pulls in the latest localization drop provided by Microsoft
-# and will update the localization templates under "..\src\localization\templates".
-# It cannot be run by someone who does not have access to Microsoft's localization drops.
+# This script pulls in the artifacts from the Azure Pipeline `powerquery-parser-1loc`
+# and will update the localization templates under `..\src\localization\templates`.
 
 import os
 import shutil
 import sys
 
-LOCALIZATION_DIR = r"\\simpleloc\drops\Drops\PowerQuery_parser_2490"
-POWERQUERY_PARSER_DIR = os.path.join(
+if len(sys.argv) == 1:
+    raise Exception("Expected a single command line argument which points to the artifacts from the `powerquery-parser-1loc` pipeline.")
+elif len(sys.argv) > 2:
+    raise Exception("Expected a single command line argument.")
+
+LOCALIZATION_DROP_DIR = sys.argv[1]
+LOCALIZATION_DIR = os.path.join(LOCALIZATION_DROP_DIR, "loc")
+POWERQUERY_PARSER_DIR = os.path.abspath(os.path.join(
     "..",
     os.path.dirname(os.path.dirname(__file__)),
-)
+))
 
-latest_dir = None
 for entry in os.scandir(LOCALIZATION_DIR):
-    if not entry.is_dir():
-        continue
-    elif latest_dir is None or latest_dir.name < entry.name:
-        latest_dir = entry
-
-assert latest_dir is not None
-
-with open("localization_drop", "r") as f:
-    if f.read() == latest_dir.name:
-        print("Already picked up {}, exiting.".format(latest_dir.name))
-        sys.exit(0)
-
-print("Picking up a new version, {}".format(latest_dir.name))
-
-LOCALIZATION_DROP_DIR = os.path.join(
-    latest_dir,
-    "BinDrops",
-    "Windows",
-    "bin",
-)
-
-for entry in os.scandir(LOCALIZATION_DROP_DIR):
+    # Ignore files.
     if not entry.is_dir():
         continue
 
+    # There are a few build artifact folders we want to ignore.
     localization_code = os.path.basename(entry.path)
-    if localization_code == "qps-ploc":
+    if localization_code in ("ResponseFiles", "en"):
         continue
 
+    # Create the filepath to where we expect the translated file to be at,
+    # and error out if it doesn't exist.
     localization_src = os.path.join(
-        LOCALIZATION_DROP_DIR,
+        LOCALIZATION_DIR,
         localization_code,
         "src",
         "localization",
         "templates",
         "en-US.json"
     )
-
     if not os.path.isfile(localization_src):
-        raise Exception("couldn't find localization file for {} at {}".format(localization_code))
+        raise Exception("couldn't find localization file for {} at {}".format(localization_code, localization_src))
 
+    # Create the filepath to where we expect the translated file to be placed.
     localization_dst = os.path.join(
         POWERQUERY_PARSER_DIR,
         "src",
@@ -65,6 +51,3 @@ for entry in os.scandir(LOCALIZATION_DROP_DIR):
 
     print("Copying {}\n\tsrc={}\n\tdst={}".format(localization_code, localization_src, localization_dst))
     shutil.copyfile(localization_src, localization_dst)
-
-with open("localization_drop", "w") as f:
-    f.write(latest_dir.name)
