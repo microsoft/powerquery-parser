8cf652857080a457e2925de0f8800211dec0e2e6
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-03-25 12:44:56 -0700

    Dedupe functions found in powerquery-language-services (#247)

--- src/powerquery-parser/language/type/index.ts
+++ src/powerquery-parser/language/type/index.ts
@@ -2,6 +2,4 @@
 // Licensed under the MIT license.
 
 export * as Type from "./type";
-export * as TypeInspector from "./inspector";
 export * as TypeUtils from "./typeUtils";
-export * from "./externalType";
