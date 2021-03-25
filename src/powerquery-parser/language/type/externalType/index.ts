8389fcad277d45a48fbd7680a920bd324f791645
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-03-25 12:06:56 -0700

    initial commit

--- src/powerquery-parser/language/type/externalType/index.ts
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export * as ExternalType from "./externalType";
-export * as ExternalTypeUtils from "./externalTypeUtils";
