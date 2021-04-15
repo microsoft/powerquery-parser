63060d1688dcc414fe5fba6691c97ac4b08dd91f
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-04-15 15:09:18 -0700

    hopefully final commit

--- src/powerquery-parser/language/ast/astUtils/index.ts
+++ src/powerquery-parser/language/ast/astUtils/index.ts
@@ -2,3 +2,4 @@
 // Licensed under the MIT license.
 
 export * from "./assert";
+export * from "./typeGuards";
--- src/powerquery-parser/language/ast/astUtils/typeGuards.ts
+++ src/powerquery-parser/language/ast/astUtils/typeGuards.ts
@@ -203,6 +203,67 @@ export function isTableType(node: Ast.TNode): node is Ast.TableType {
     return node.kind === Ast.NodeKind.TableType;
 }
 
+export function isTArrayWrapper(node: Ast.TNode): node is Ast.TArrayWrapper {
+    return node.kind === Ast.NodeKind.ArrayWrapper;
+}
+
+export function isTBinOpExpression(node: Ast.TNode): node is Ast.TBinOpExpression {
+    return isTBinOpExpressionKind(node.kind);
+}
+
+export function isTBinOpExpressionKind(nodeKind: Ast.NodeKind): nodeKind is Ast.TBinOpExpressionNodeKind {
+    switch (nodeKind) {
+        case Ast.NodeKind.ArithmeticExpression:
+        case Ast.NodeKind.AsExpression:
+        case Ast.NodeKind.EqualityExpression:
+        case Ast.NodeKind.IsExpression:
+        case Ast.NodeKind.LogicalExpression:
+        case Ast.NodeKind.MetadataExpression:
+        case Ast.NodeKind.RelationalExpression:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+export function isTConstant(node: Ast.TNode): node is Ast.TConstant {
+    return node.kind === Ast.NodeKind.Constant;
+}
+
+export function isTCsv(node: Ast.TNode): node is Ast.TCsv {
+    return node.kind === Ast.NodeKind.Csv;
+}
+
+export function isTKeyValuePair(node: Ast.TNode): node is Ast.TKeyValuePair {
+    switch (node.kind) {
+        case Ast.NodeKind.GeneralizedIdentifierPairedAnyLiteral:
+        case Ast.NodeKind.GeneralizedIdentifierPairedExpression:
+        case Ast.NodeKind.IdentifierPairedExpression:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+export function isTPairedConstant(node: Ast.TNode): node is Ast.TPairedConstant {
+    switch (node.kind) {
+        case Ast.NodeKind.AsNullablePrimitiveType:
+        case Ast.NodeKind.AsType:
+        case Ast.NodeKind.ErrorRaisingExpression:
+        case Ast.NodeKind.IsNullablePrimitiveType:
+        case Ast.NodeKind.NullablePrimitiveType:
+        case Ast.NodeKind.NullableType:
+        case Ast.NodeKind.OtherwiseExpression:
+        case Ast.NodeKind.TypePrimaryType:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
 export function isTypePrimaryType(node: Ast.TNode): node is Ast.TypePrimaryType {
     return node.kind === Ast.NodeKind.TypePrimaryType;
 }
