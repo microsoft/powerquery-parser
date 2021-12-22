c795d318d51370bcfacb2f18bb682c1640685860
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-12-22 12:58:02 -0600

    reworked how benchmark logs are created

---
+++ src/test/resourceTest/benchmark/createBenchmarks.ts
@@ -0,0 +1,48 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { TestFileUtils } from "../../testUtils";
+
+import { DefaultSettings, Settings } from "../../..";
+import { BenchmarkTraceManager } from "../../../powerquery-parser/common/trace";
+
+const NumberOfRunsPerFile: number = 100;
+const ResourceDirectory: string = path.dirname(__filename);
+const SourceFilesDirectory: string = path.join(ResourceDirectory, "sourceFiles");
+const OutputDirectory: string = path.join(ResourceDirectory, "logs");
+
+function createOutputStream(filePath: string, iteration: number): fs.WriteStream {
+    const iterationFilePath: string = path.join(
+        OutputDirectory,
+        `file_${path.parse(filePath).name}_iteration_${iteration}.log`,
+    );
+
+    // tslint:disable-next-line: non-literal-fs-path
+    if (!fs.existsSync(OutputDirectory)) {
+        // tslint:disable-next-line: non-literal-fs-path
+        fs.mkdirSync(OutputDirectory, { recursive: true });
+    }
+
+    return fs.createWriteStream(iterationFilePath, { flags: "w" });
+}
+
+for (const filePath of TestFileUtils.getPowerQueryFilesRecursively(SourceFilesDirectory)) {
+    console.log(`Starting benchmarks for ${filePath}`);
+
+    for (let iteration: number = 0; iteration < NumberOfRunsPerFile; iteration += 1) {
+        const stream: fs.WriteStream = createOutputStream(filePath, iteration);
+
+        stream.on("open", () => {
+            if (iteration % 10 === 0) {
+                console.log(`\tRunning iteration ${iteration} out of ${NumberOfRunsPerFile}`);
+            }
+
+            const benchmarkSettings: Settings = {
+                ...DefaultSettings,
+                traceManager: new BenchmarkTraceManager((message: string) => stream.write(message)),
+            };
+
+            TestFileUtils.tryLexParse(benchmarkSettings, filePath);
+        });
+    }
+}
