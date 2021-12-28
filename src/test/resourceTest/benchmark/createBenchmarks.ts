83ff020a19a7d69f67b901e4ae06e313b72a711e
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-12-27 23:57:25 -0600

    updating stdout

--- src/test/resourceTest/benchmark/createBenchmarks.ts
+++ src/test/resourceTest/benchmark/createBenchmarks.ts
@@ -6,7 +6,7 @@ import { TestFileUtils } from "../../testUtils";
 import { DefaultSettings, Settings } from "../../..";
 import { BenchmarkTraceManager } from "../../../powerquery-parser/common/trace";
 
-const NumberOfRunsPerFile: number = 100;
+const NumberOfRunsPerFile: number = 10;
 const ResourceDirectory: string = path.dirname(__filename);
 const SourceFilesDirectory: string = path.join(ResourceDirectory, "sourceFiles");
 const OutputDirectory: string = path.join(ResourceDirectory, "logs");
@@ -27,14 +27,14 @@ function createOutputStream(filePath: string, iteration: number): fs.WriteStream
 }
 
 for (const filePath of TestFileUtils.getPowerQueryFilesRecursively(SourceFilesDirectory)) {
-    console.log(`Starting benchmarks for ${filePath}`);
-
     for (let iteration: number = 0; iteration < NumberOfRunsPerFile; iteration += 1) {
         const stream: fs.WriteStream = createOutputStream(filePath, iteration);
 
         stream.on("open", () => {
-            if (iteration % 10 === 0) {
-                console.log(`\tRunning iteration ${iteration} out of ${NumberOfRunsPerFile}`);
+            if (iteration % 10 === 0 || iteration === NumberOfRunsPerFile - 1) {
+                console.log(
+                    `Running iteration ${iteration + 1} out of ${NumberOfRunsPerFile} for ${path.basename(filePath)}`,
+                );
             }
 
             const benchmarkSettings: Settings = {
