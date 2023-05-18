f7c2a64d648ac595e353a2819ea8dddf62ac88f3
JordanBoltonMN <jordanboltonmn@gmail.com>
2023-05-18 17:29:56 -0500

    removed debug code

--- src/powerquery-parser/common/typeScriptTypeUtils.ts
+++ src/powerquery-parser/common/typeScriptTypeUtils.ts
@@ -1,13 +1,9 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-export type ExtractFirstGenericType<T> = T extends Generic<infer U> ? U : never;
-
 // removes `readonly` from T's attributes
 export type StripReadonly<T> = { -readonly [K in keyof T]: T[K] };
 
 export function isDefined<T>(x: T | undefined): x is T {
     return x !== undefined;
 }
-
-type Generic<T> = { value: T };
