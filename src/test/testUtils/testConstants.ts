b95d081c7809a822d1202e6818ffaafb368592be
JordanBoltonMN <jordanboltonmn@gmail.com>
2023-05-24 14:43:16 -0500

    adding V2 as default parser

--- src/test/testUtils/testConstants.ts
+++ src/test/testUtils/testConstants.ts
@@ -5,5 +5,6 @@ import { Parser } from "../../powerquery-parser";
 
 export const ParserByParserName: ReadonlyMap<string, Parser.Parser> = new Map([
     ["CombinatorialParser", Parser.CombinatorialParser],
+    ["CombinatorialParserV2", Parser.CombinatorialParserV2],
     ["RecursiveDescentParser", Parser.RecursiveDescentParser],
 ]);
