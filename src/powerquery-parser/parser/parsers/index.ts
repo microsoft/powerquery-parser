901555c0acce3d588dc7270389799e30ab905404
JordanBoltonMN <jordanboltonmn@gmail.com>
2023-06-04 22:42:14 -0500

    remove v1

--- src/powerquery-parser/parser/parsers/index.ts
+++ src/powerquery-parser/parser/parsers/index.ts
@@ -5,5 +5,5 @@ import * as NaiveParseSteps from "./naiveParseSteps";
 
 export * from "./combinatorialParserV2";
 export { NaiveParseSteps };
-export { CombinatorialParser } from "./combinatorialParserV1";
+export { CombinatorialParserV2 } from "./combinatorialParserV2";
 export { RecursiveDescentParser } from "./recursiveDescentParser";
